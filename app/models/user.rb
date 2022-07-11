7c0a55f0931503386780473d11e0090a20be48e0
takumi-1016 <tn1016recruit@gmail.com>
2022-07-11 14:35:18 +0900

    ログイン機能の実装の途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_one_attached :image
     has_many :rooms, class_name: "room", foreign_key: "reference_id", dependent: :destroy
     has_many :reservations, class_name: "reservation", foreign_key: "reference_id", dependent: :destroy
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#top'
+
+
   resources :users
+
+
   resources :rooms  
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get 'home/room_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
