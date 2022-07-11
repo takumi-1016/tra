ea9d11b8bbad09024c727c9978b4dfb8274502bc
takumi-1016 <tn1016recruit@gmail.com>
2022-07-11 09:43:13 +0900

    userとroomとトップ画面のコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'home#top'
+  resources :users
+  resources :rooms  
+  get 'home/room_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
